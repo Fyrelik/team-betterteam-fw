082c7d41e4bcbb334cb5f619b6d79d09337db807
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-10-05 00:14:08 +0000

    created findings

--- scripts/grep-keys.sh
+++ scripts/grep-keys.sh
@@ -1,1 +1,2 @@
+echo '''grep -R --line-number --exclude-dir=js  -iE "root:|:x|:\!|BEGIN RSA PRIVATE KEY|BEGIN PRIVATE KEY|ssh-rsa" "$1" | sed -n '1,200p''''
 grep -R --line-number --exclude-dir=js  -iE "root:|:x|:\!|BEGIN RSA PRIVATE KEY|BEGIN PRIVATE KEY|ssh-rsa" "$1" | sed -n '1,200p'
