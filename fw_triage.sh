24f9c0c97abf35717a16231a52e3297a915541b6
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-10-05 19:05:44 -0500

    added to script

--- fw_triage.sh
+++ fw_triage.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(readlink -f "$0" | xargs dirname)/scripts"
-echo $SCRIPT_DIR
+
+function source_script() {
+  if ! [ -f "$SCRIPT_DIR/$1.sh" ]; then
+    echo "'$SCRIPT_DIR/$1.sh' not found"
+    exit 1
+  fi
+  source "$SCRIPT_DIR/$1.sh"
+}
+
+source_script header
+source_script dependency_check
+
--- scripts/dependency_check.sh
+++ scripts/dependency_check.sh
@@ -1,16 +1,33 @@
+#!/bin/bash
+
 DEPENDENCIES=(
     "tree"
+    "binwalk"
 )
 
-DEP_MISSING=0
+MISSING=()
 
 for dep in "${DEPENDENCIES[@]}"; do
     if ! (which "$dep" >> /dev/null); then
-        echo "DEPENDANCY MISSING: $dep"
-        DEP_MISSING=1
+        MISSING+=("$dep")
     fi
 done
 
-if (( DEP_MISSING = 1 )); then
+if [ ${#MISSING[@]} -gt 0 ]; then
+  IN=""
+  while [ "$IN" != "Y" ] && [ "$IN" != "n" ]; do
+    echo -n "${#MISSING[@]} dependencies missing. Install them? (requires sudo) [Y/n]: "
+    read -r IN
+  done
+  if [ "$IN" == "Y" ]; then
+    for dep in "${MISSING[@]}"; do
+      if ! (sudo apt install -y "$dep"); then
+        echo "Failed to install $dep"
+        exit 1
+      fi
+    done
+  else
+    echo "Cannot run without dependencies, exiting..."
     exit 1
+  fi
 fi
