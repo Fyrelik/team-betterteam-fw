cb206287d89d284a290abda84c597797533dbc66
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-10-05 21:19:32 -0500

    added input identification

--- fw_triage.sh
+++ fw_triage.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(readlink -f "$0" | xargs dirname)/scripts"
+FILE_PATH="$(readlink -f "$1")"
+FILE_NAME="$(basename "$FILE_PATH")"
+DIR_PATH="$(dirname "$FILE_PATH")"
+if ! [ -f "$FILE_PATH" ]; then
+  echo "File $FILE_PATH not found"
+  exit 1
+fi
 
+SCRIPT_DIR="$(readlink -f "$0" | xargs dirname)/scripts"
 function source_script() {
   if ! [ -f "$SCRIPT_DIR/$1.sh" ]; then
     echo "'$SCRIPT_DIR/$1.sh' not found"
