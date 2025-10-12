2122544d693b832941023ac7f7a1a099b3291f2a
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-10-12 18:49:28 +0000

    ensure apt is present

--- script_helpers/dependency_check.sh
+++ script_helpers/dependency_check.sh
@@ -39,6 +39,12 @@ if [ ${#MISSING[@]} -gt 0 ]; then
   ## do installs
   if [ "${IN,,}" == "y" ]; then
 
+    if ! (which apt); then
+        echo "ERR: apt not found, please install dependencies manually."
+        echo "${MISSING[@]}"
+        exit 1
+    fi
+
     # activate sudo
     if ! (sudo echo -n ""); then
         echo ""
